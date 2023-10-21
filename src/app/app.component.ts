8ac38fdf7d1b108039936959a2ca6f8f9197020a
Vaibhav Gupta <vaibhav101098@gmail.com>
2023-10-22 00:01:25 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'blog';
+  count = 0;
+  counter = (type:string) =>{
+    // this.count ++
+    type === 'add'? this.count++ : this.count--;
+  }
+}
